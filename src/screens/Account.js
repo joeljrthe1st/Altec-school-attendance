4983ea0ed9972c4d1f1d16cd7f2e876effae77ae
nageri <onacedricnageri@gmail.com>
2024-06-21 13:06:12 +0300

    feat: Styled the account update form

--- src/screens/Account.js
+++ src/screens/Account.js
@@ -1,14 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { Text, View, Button, TextInput } from 'react-native';
-import { auth } from '../utils/firebaseConfig'; // Ensure the correct path to your Firebase configuration
-import { fetchUserData, updateUserData } from '../utils/firebaseConfig'; // Ensure the correct path to your Firebase configuration
+import React, { useEffect, useState } from "react";
+import { Text, View, Button, TextInput, ActivityIndicator } from "react-native";
+import { auth } from "../utils/firebaseConfig"; // Ensure the correct path to your Firebase configuration
+import { fetchUserData, updateUserData } from "../utils/firebaseConfig"; // Ensure the correct path to your Firebase configuration
+import { ScrollView } from "react-native-gesture-handler";
 
 const Account = () => {
   const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [newFirstName, setNewFirstName] = useState('');
-  const [newLastName, setNewLastName] = useState('');
-  const [newPhone, setNewPhone] = useState('');
+  const [newFirstName, setNewFirstName] = useState("");
+  const [newLastName, setNewLastName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
 
   useEffect(() => {
     const getUserData = async () => {
@@ -32,30 +33,37 @@ const Account = () => {
   }, []);
 
   const handleUpdate = async () => {
-    const currentUser = auth.currentUser;
-    if (currentUser) {
-      const userId = currentUser.uid;
-      const updatedData = {};
+    try {
+      setLoading(true);
+      const currentUser = auth.currentUser;
+      if (currentUser) {
+        const userId = currentUser.uid;
+        const updatedData = {};
 
-      if (newFirstName && newFirstName !== userData.firstname) {
-        updatedData.firstname = newFirstName;
-      }
-      if (newLastName && newLastName !== userData.lastname) {
-        updatedData.lastname = newLastName;
-      }
-      if (newPhone && newPhone !== userData.phoneNumber) {
-        updatedData.phoneNumber = newPhone;
-      }
+        if (newFirstName && newFirstName !== userData.firstname) {
+          updatedData.firstname = newFirstName;
+        }
+        if (newLastName && newLastName !== userData.lastname) {
+          updatedData.lastname = newLastName;
+        }
+        if (newPhone && newPhone !== userData.phoneNumber) {
+          updatedData.phoneNumber = newPhone;
+        }
 
-      if (Object.keys(updatedData).length > 0) {
-        try {
-          await updateUserData(userId, updatedData);
-          const updatedUserData = await fetchUserData(userId);
-          setUserData(updatedUserData);
-        } catch (error) {
-          console.error("Error updating user data:", error);
+        if (Object.keys(updatedData).length > 0) {
+          try {
+            await updateUserData(userId, updatedData);
+            const updatedUserData = await fetchUserData(userId);
+            setUserData(updatedUserData);
+          } catch (error) {
+            console.error("Error updating user data:", error);
+          }
         }
       }
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -70,38 +78,57 @@ const Account = () => {
   if (!userData) {
     return (
       <View className="flex-1 justify-center items-center bg-white-100">
-        <Text className="text-2xl font-bold text-red-500">No user data available</Text>
+        <Text className="text-2xl font-bold text-red-500">
+          No user data available
+        </Text>
       </View>
     );
   }
 
   return (
-    <View className="flex-1 justify-center items-center bg-white-100">
-      <Text className="text-xl">First Name: {userData.firstname}</Text>
-      <Text className="text-xl">Last Name: {userData.lastname}</Text>
-      <Text className="text-xl">Phone Number: {userData.phoneNumber}</Text>
-      <Text className="text-xl">Email: {userData.email}</Text>
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} className="bg-white">
+      <View className="flex-1 justify-center items-center bg-white-100">
+        <Text className="w-80 p-4 m-2 bg-gray-200 text-justify">
+          First Name: {userData.firstname}
+        </Text>
+        <Text className="w-80 p-4 m-2 bg-gray-200 text-justify">
+          Last Name: {userData.lastname}
+        </Text>
+        <Text className="w-80 p-4 m-2 bg-gray-200 text-justify">
+          Phone Number: {userData.phoneNumber}
+        </Text>
+        <Text className="w-80 p-4 m-2 bg-gray-200 text-justify">
+          Email: {userData.email}
+        </Text>
 
-      <TextInput
-        placeholder="New First Name"
-        value={newFirstName}
-        onChangeText={setNewFirstName}
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10 }}
-      />
-      <TextInput
-        placeholder="New Last Name"
-        value={newLastName}
-        onChangeText={setNewLastName}
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10 }}
-      />
-      <TextInput
-        placeholder="New Phone Number"
-        value={newPhone}
-        onChangeText={setNewPhone}
-        style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginBottom: 10 }}
-      />
-      <Button title="Update account" onPress={handleUpdate} />
-    </View>
+        <View className="w-80 flex-row items-center border border-gray-300 rounded px-3 py-2 mb-4 mt-1">
+          <TextInput
+            placeholder="New First Name"
+            value={newFirstName}
+            onChangeText={setNewFirstName}
+          />
+        </View>
+        <View className="w-80 flex-row items-center border border-gray-300 rounded px-3 py-2 mb-4">
+          <TextInput
+            placeholder="New Last Name"
+            value={newLastName}
+            onChangeText={setNewLastName}
+          />
+        </View>
+        <View className="w-80 flex-row items-center border border-gray-300 rounded px-3 py-2 mb-4">
+          <TextInput
+            placeholder="New Phone Number"
+            value={newPhone}
+            onChangeText={setNewPhone}
+          />
+        </View>
+        {loading ? (
+          <ActivityIndicator size="large" color="#1e40af" />
+        ) : (
+          <Button title="Update account" onPress={handleUpdate} />
+        )}
+      </View>
+    </ScrollView>
   );
 };
 
